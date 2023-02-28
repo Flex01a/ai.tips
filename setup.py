4a39d433c871c9747630fc965c006fe9e3949efb
Flex01a <91494774+Flex01a@users.noreply.github.com>
2023-02-28 22:47:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages, setup
 setup(
-    name='flexy_ai',
+    name='flexy-ai',
     packages=find_packages(include=['tips']),
     version='0.1.0',
     description='Library Of Flexy AI Functions',
     author='Flexy',
     license='MIT',
     install_requires=[],
-    setup_requires=['flexy_ai'],
-    tests_require=['flexy_ai==0.1.0'],
+    setup_requires=['flexy-ai'],
+    tests_require=['flexy-ai==0.1.0'],
     test_suite='tips',
 )

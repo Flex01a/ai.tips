75f501c15392baf5ccb9d42894b8cba064a4010e
Flex01a <91494774+Flex01a@users.noreply.github.com>
2023-02-28 22:01:15 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='ai_flexy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Library Of Flexy AI Functions',
+    author='Flexy',
+    license='MIT',
+)
